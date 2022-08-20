e99e869a59807ac26cc4a63e56f2ec5f0fe659e1
gideonlangat98 <gideonlangat98@gmail.com>
2022-08-20 10:53:44 +0300

    commit tree 30a01d875fb576cacfbc080216ead196f4fce7e8

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
 
+    #setters method
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    #getters method
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
 end
