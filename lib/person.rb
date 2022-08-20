95d17f473a650fc7fd2ab7a862c9441ef7aab5c1
gideonlangat98 <gideonlangat98@gmail.com>
2022-08-20 10:53:56 +0300

    Done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
 
+    #setters method
+    def name=(name)
+        @name = name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    #getters method
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
 end
